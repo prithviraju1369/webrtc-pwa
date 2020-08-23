fc8a5912d9afa287b131959f4498fd3cd00af93e
Uppalapati, Prithvi Raju (US - Hyderabad) <prituppalapati@deloitte.com>
2020-08-23 21:52:52 +0530

    latest

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined);
+const myPeer = new Peer({
+  config: {'iceServers': [
+    { url: 'stun:stun.l.google.com:19302' },
+    { url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
+  ]} /* Sample servers, please use appropriate ones */
+});
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
