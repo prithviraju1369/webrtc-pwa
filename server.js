979f78041bd6751094af06b252b42af8efb1dbc5
Uppalapati, Prithvi Raju (US - Hyderabad) <prituppalapati@deloitte.com>
2020-08-23 22:33:57 +0530

    latest

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room, ice: JSON.stringify(ice) })
+  res.render('room', { roomId: req.params.room, ice: ice })
 })
 
 io.on('connection', socket => {
