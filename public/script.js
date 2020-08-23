22a7737a36a93156426d2cc29673770e22e1ab70
Uppalapati, Prithvi Raju (US - Hyderabad) <prituppalapati@deloitte.com>
2020-08-23 13:54:06 +0530

    latest

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined);
 const myVideo = document.createElement('video')
-
+myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
